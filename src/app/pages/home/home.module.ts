adacda569d8b2810450e8c76eb31a7faf66c3c46
iSeth1998 <duvanbradbrid@gmail.com>
2021-01-10 14:55:44 -0500

    Carousel agregado en la pantalla de inicio

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
 import { HomeRoutingModule } from './home-routing.module';
+import { CarouselModule } from 'src/app/shared/carousel/carousel.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     LayoutModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselModule
   ]
 })
 export class HomeModule { }
